0c4c9a42f7fb5579d80e5119a342f83790a2e641
Nurbek <nurbek.saitqulov@gmail.com>
2022-11-23 14:00:38 +0500

    if06.py

--- if06.py
+++ if06.py
@@ -6,4 +6,33 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a=n%10
+    n//=10
+
+    b=n%10
+    n//=10
+
+    c=n%10
+    n//=10
+
+    d=n%10
+    n//=10
+
+    e=n%10
+
+    if a>b and a>c and a>d and a>e:
+        return len(str(a))
+    
+    elif b>a and b>c and b>d and b>e:
+        return len(str(b*10+a))
+    
+    elif c>a and c>b and c>d and c>e:
+        return len(str(c*100+b*10+a))
+    
+    elif d>a and d>b and d>c and d>e:
+        return len(str(d*1000+c*100+b*10+a))
+    
+    elif e>a and e>b and e>c and e>d:
+        return len(str(e*10000+d*1000+c*100+b*10+a))
+
+print(main(54694))
